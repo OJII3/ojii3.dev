65572813ed0b27da4d177482a832d5700cb1a1f6
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-07-16 21:02:32 +0900

    update: links

--- src/app/_components/About/index.tsx
+++ src/app/_components/About/index.tsx
@@ -1,74 +1,74 @@
-import { TimelineItem, TimelineRoot } from "@/components/ui/timeline";
 import {
-	Badge,
-	Center,
-	Collapsible,
-	Container,
-	Heading,
-	TimelineConnector,
-	TimelineContent,
-	TimelineTitle,
+  Badge,
+  Center,
+  Collapsible,
+  Container,
+  Heading,
+  TimelineConnector,
+  TimelineContent,
+  TimelineTitle,
 } from "@chakra-ui/react";
 import { LuCheck } from "react-icons/lu";
+import { TimelineItem, TimelineRoot } from "@/components/ui/timeline";
 
 const items = [
-	{ date: "2022.04", title: "東京農工大学 工学部 機械システム工学科 入学" },
-	{
-		date: "2022.12",
-		title: "フロントエンドエンジニア長期インターンシップ",
-	},
-	{ date: "2024.03", title: "東京農工大学 工学部 機械システム工学科 中退" },
-	{
-		date: "2024.04",
-		title: "東京農工大学 工学部 知能情報システム工学科 編入学",
-	},
- {
-  date: "2024.04",
-  title: "MCC部長就任",
- },
-	{
-		date: "2024.06",
-		title: "NHK学生ロボコン2024 準決勝進出",
-	},
-	{
-		date: "2024.11",
-		title: "技育展2024決勝大会出場",
-	},
- {
-  date: "2025.02",
-  title: "技育博vol6 DeNA賞獲得",
- },
+  { date: "2022.04", title: "東京農工大学 工学部 機械システム工学科 入学" },
+  {
+    date: "2022.12",
+    title: "フロントエンドエンジニア長期インターンシップ",
+  },
+  { date: "2024.03", title: "東京農工大学 工学部 機械システム工学科 中退" },
+  {
+    date: "2024.04",
+    title: "東京農工大学 工学部 知能情報システム工学科 編入学",
+  },
+  {
+    date: "2024.04",
+    title: "MCC部長就任",
+  },
+  {
+    date: "2024.06",
+    title: "NHK学生ロボコン2024 準決勝進出",
+  },
+  {
+    date: "2024.11",
+    title: "技育展2024決勝大会出場",
+  },
+  {
+    date: "2025.02",
+    title: "技育博vol6 DeNA賞獲得",
+  },
 ];
 
 export const About = () => {
-	return (
-		<Center minH="80" flexDir="column" maxW="800px" mx="auto" gap="4">
-			<Heading>About Me</Heading>
-			<p>Linux, Neovim, Blender, Unity, TypeScript などが好き。</p>
-			<Collapsible.Root>
-				<Collapsible.Trigger as={Center}>
-					The tracks I left behind
-				</Collapsible.Trigger>
-				<Collapsible.Content p="4">
-					<Container>
-						<TimelineRoot>
-							{items.map((item) => (
-								<TimelineItem key={item.title}>
-									<TimelineConnector>
-										<LuCheck />
-									</TimelineConnector>
-									<TimelineContent>
-										<TimelineTitle>
-											<Badge>{item.date}</Badge>
-											{item.title}
-										</TimelineTitle>
-									</TimelineContent>
-								</TimelineItem>
-							))}
-						</TimelineRoot>
-					</Container>
-				</Collapsible.Content>
-			</Collapsible.Root>
-		</Center>
-	);
+  return (
+    <Center minH="80" flexDir="column" maxW="800px" mx="auto" gap="4">
+      <Heading>About Me</Heading>
+      <p>Linux, Neovim, Blender, Unity, TypeScript などが好き。</p>
+      <Collapsible.Root>
+        <Collapsible.Trigger as={Center}>
+          The tracks I left behind
+        </Collapsible.Trigger>
+        <Collapsible.Content p="4">
+          <Container>
+            <TimelineRoot>
+              {items.map((item) => (
+                <TimelineItem key={item.title}>
+                  <TimelineConnector>
+                    <LuCheck />
+                  </TimelineConnector>
+                  <TimelineContent>
+                    <TimelineTitle>
+                      <Badge>{item.date}</Badge>
+                      {item.title}
+                    </TimelineTitle>
+                  </TimelineContent>
+                </TimelineItem>
+              ))}
+            </TimelineRoot>
+          </Container>
+        </Collapsible.Content>
+      </Collapsible.Root>
+    </Center>
+  );
 };
--- src/app/_components/Drawer/_components/DrawerContainer.tsx
+++ src/app/_components/Drawer/_components/DrawerContainer.tsx
@@ -1,15 +1,16 @@
 import { Button } from "@/components/ui/button";
 import {
-	DrawerBackdrop,
-	DrawerBody,
-	DrawerContent,
-	DrawerHeader,
-	DrawerRoot,
-	DrawerTitle,
-	DrawerTrigger,
+  DrawerBackdrop,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerRoot,
+  DrawerTitle,
+  DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Box, DrawerActionTrigger, Flex, IconButton } from "@chakra-ui/react";
 import { BsBook, BsCode, BsGithub, BsTwitterX } from "react-icons/bs";
+import { SiPixiv } from "react-icons/si";
 import { LuMenu } from "react-icons/lu";
 
 import DiagonalSlash from "@/assets/diagonal_slash.svg";
@@ -21,145 +22,150 @@ import { AnimatedLink } from "./AnimatedLink";
 import { AnimatedVerticalBar } from "./AnimatedVerticalBar";
 
 const links = [
-	{
-		href: "https://x.com/ojii3dev",
-		label: "TwitterX",
-		icon: <BsTwitterX size="22" />,
-	},
-	{
-		href: "https://github.com/ojii3",
-		label: "GitHub",
-		icon: <BsGithub size="22" />,
-	},
-	{
-		href: "https://atcoder.jp/users/ojii3",
-		icon: <BsCode size="22" />,
-		label: "AtCoder",
-	},
-	{
-		href: "https://honkaiimpact3.hoyoverse.com",
-		icon: <Honkai3rd />,
-		label: "Honkai Impact 3rd",
-	},
-	{
-		href: "https://blog.ojii3.dev",
-		icon: <BsBook size="22" />,
-		label: "Blog",
-	},
+  {
+    href: "https://x.com/ojii3dev",
+    label: "TwitterX",
+    icon: <BsTwitterX size="22" />,
+  },
+  {
+    href: "https://github.com/ojii3",
+    label: "GitHub",
+    icon: <BsGithub size="22" />,
+  },
+  {
+    href: "https://atcoder.jp/users/ojii3",
+    icon: <BsCode size="22" />,
+    label: "AtCoder",
+  },
+  {
+    href: "https://www.pixiv.net/users/91784105",
+    icon: <SiPixiv size="22" />,
+    label: "Pixiv",
+  },
+  {
+    href: "https://honkaiimpact3.hoyoverse.com",
+    icon: <Honkai3rd />,
+    label: "Honkai Impact 3rd",
+  },
+  {
+    href: "https://blog.ojii3.dev",
+    icon: <BsBook size="22" />,
+    label: "Blog",
+  },
 ];
 
 export const DrawerContainer = () => {
-	return (
-		<>
-			<DrawerRoot size="xs">
-				<DrawerBackdrop />
-				<DrawerTrigger asChild>
-					<Button
-						pos="fixed"
-						top="2"
-						right="2"
-						bg="orange"
-						h="auto"
-						p="1"
-						color="gray.900"
-						shadow="xs"
-					>
-						<Box
-							shadow="xs"
-							h="6"
-							w="8"
-							bg="gray.200"
-							pl="3"
-							pr="2"
-							clipPath="polygon(0 0, 0 calc(100% - 14px), 14px 100%, 100% 100%, 100% 0)"
-							color="gray.700"
-						>
-							<LuMenu />
-						</Box>
-					</Button>
-				</DrawerTrigger>
+  return (
+    <>
+      <DrawerRoot size="xs">
+        <DrawerBackdrop />
+        <DrawerTrigger asChild>
+          <Button
+            pos="fixed"
+            top="2"
+            right="2"
+            bg="orange"
+            h="auto"
+            p="1"
+            color="gray.900"
+            shadow="xs"
+          >
+            <Box
+              shadow="xs"
+              h="6"
+              w="8"
+              bg="gray.200"
+              pl="3"
+              pr="2"
+              clipPath="polygon(0 0, 0 calc(100% - 14px), 14px 100%, 100% 100%, 100% 0)"
+              color="gray.700"
+            >
+              <LuMenu />
+            </Box>
+          </Button>
+        </DrawerTrigger>
 
-				<DrawerContent
-					clipPath="polygon(0 0, 0 30%, 40px calc(30% + 40px), 40px calc(100% - 160px), 0 calc(100% - 120px), 0 100%, 100% 100%, 100% 0)"
-					pl="12"
-					bg="gray.800"
-					color="gray.100"
-				>
-					<Box pos="absolute" left="4" zIndex="-1" color="orange">
-						<Box h="16vh" w="1" bg="orange" />
-						<Box mt="-2">
-							<DiagonalSlash />
-						</Box>
-					</Box>
+        <DrawerContent
+          clipPath="polygon(0 0, 0 30%, 40px calc(30% + 40px), 40px calc(100% - 160px), 0 calc(100% - 120px), 0 100%, 100% 100%, 100% 0)"
+          pl="12"
+          bg="gray.800"
+          color="gray.100"
+        >
+          <Box pos="absolute" left="4" zIndex="-1" color="orange">
+            <Box h="16vh" w="1" bg="orange" />
+            <Box mt="-2">
+              <DiagonalSlash />
+            </Box>
+          </Box>
 
-					<Box
-						pos="absolute"
-						top="4"
-						bottom="-12px"
-						color="gray.500"
-						right="8"
-						opacity="10%"
-						zIndex="-1"
-						h="100%"
-					>
-						<ForKiana />
-					</Box>
-					<AnimatedVerticalBar />
-					<Box
-						pos="absolute"
-						bottom="9"
-						left="20"
-						maxH="30%"
-						color="orange"
-						zIndex="-1"
-					>
-						<DiagonalWindow />
-					</Box>
+          <Box
+            pos="absolute"
+            top="4"
+            bottom="-12px"
+            color="gray.500"
+            right="8"
+            opacity="10%"
+            zIndex="-1"
+            h="100%"
+          >
+            <ForKiana />
+          </Box>
+          <AnimatedVerticalBar />
+          <Box
+            pos="absolute"
+            bottom="9"
+            left="20"
+            maxH="30%"
+            color="orange"
+            zIndex="-1"
+          >
+            <DiagonalWindow />
+          </Box>
 
-					<DrawerActionTrigger asChild>
-						<IconButton bg="transparent" pos="absolute" right="2" top="2">
-							<IoClose />
-						</IconButton>
-					</DrawerActionTrigger>
+          <DrawerActionTrigger asChild>
+            <IconButton bg="transparent" pos="absolute" right="2" top="2">
+              <IoClose />
+            </IconButton>
+          </DrawerActionTrigger>
 
-					<DrawerHeader>
-						<Box
-							pos="absolute"
-							top="0"
-							left="10"
-							h="16"
-							w="44"
-							bg="orange"
-							zIndex="-1"
-							clipPath="polygon(0 0, 0 calc(100% - 24px), 24px 100%, calc(100% - var(--chakra-spacing-16)) 100%, 100% 0)"
-						/>
-						<DrawerTitle
-							fontFamily="orbitron"
-							letterSpacing="0.1em"
-							color="gray.700"
-						>
-							Links
-						</DrawerTitle>
-					</DrawerHeader>
-					<DrawerBody
-						as={Flex}
-						gap="3"
-						flexDir="column"
-						pt="12"
-						overflowX="hidden"
-					>
-						{links.map((link, i) => (
-							<AnimatedLink
-								key={link.href}
-								href={link.href}
-								icon={link.icon}
-								label={link.label}
-								delay={(i + 0.05) * 0.1}
-							/>
-						))}
-					</DrawerBody>
-				</DrawerContent>
-			</DrawerRoot>
-		</>
-	);
+          <DrawerHeader>
+            <Box
+              pos="absolute"
+              top="0"
+              left="10"
+              h="16"
+              w="44"
+              bg="orange"
+              zIndex="-1"
+              clipPath="polygon(0 0, 0 calc(100% - 24px), 24px 100%, calc(100% - var(--chakra-spacing-16)) 100%, 100% 0)"
+            />
+            <DrawerTitle
+              fontFamily="orbitron"
+              letterSpacing="0.1em"
+              color="gray.700"
+            >
+              Links
+            </DrawerTitle>
+          </DrawerHeader>
+          <DrawerBody
+            as={Flex}
+            gap="3"
+            flexDir="column"
+            pt="12"
+            overflowX="hidden"
+          >
+            {links.map((link, i) => (
+              <AnimatedLink
+                key={link.href}
+                href={link.href}
+                icon={link.icon}
+                label={link.label}
+                delay={(i + 0.05) * 0.1}
+              />
+            ))}
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerRoot>
+    </>
+  );
 };
